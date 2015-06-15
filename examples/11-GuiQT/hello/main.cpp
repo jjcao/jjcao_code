e389ff45173003f533165de71373c884207b4bf2
JJCAO <jjcao1231@gmail.com>
2015-06-15 11:27:46 -0700

    no message

--- examples/11-GuiQT/hello/main.cpp
+++ examples/11-GuiQT/hello/main.cpp
@@ -1,10 +1,21 @@
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QLabel>
+#include <QtWidgets/qmessagebox.h>
+#include <qtimer.h>
 
 int main( int argc, char **argv )
 {
     QApplication app( argc, argv );
     QLabel l( "Hello World!" );
-    l.show();
+	l.show();
+
+	{
+		QMessageBox tbox;
+		tbox.setText(QString("%1 ms - cost = %2").arg(11).arg(22));
+		//tbox.setStandardButtons(QMessageBox::Ok);
+		tbox.exec();
+		//tbox.show(); //QTimer::singleShot(60, &tbox, SLOT(hide()));
+	}
+
     return app.exec();
 }
